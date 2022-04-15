571a09550052f422fa8c90a8ec4490c2974f8fe1
Aidan Border <planes4u@outlook.com>
2022-04-15 12:51:59 -0400

    Adding header and notes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/* Aidan Border
+* CSC 3380
+* Module 5 Assignment 1
+* Part 1: Implement & Compare Topographical Sort Algorithms
+* NOTE: Use something smaller than seconds when comparing your algorithms. You'll get 0 seconds for both algorithms
+* if you use seconds, which is obviously unhelpful. */
+
 public class Main {
 
     public static void main(String[] args) {
